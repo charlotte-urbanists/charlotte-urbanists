1de956f695716f2492ba4ae907bc82cb45ea5246
Rahul <rahul@rsun.dev>
2022-02-18 19:44:39 -0500

    fix broken icon

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,16 +2,18 @@
  * This component lists all the projects in the index page
  */
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBus, faWalking } from '@fortawesome/free-solid-svg-icons';
 
 export default function Projects() {
   const projects = [
     {
-      icon: 'fas fa-bus icon',
+      icon: faBus,
       title: 'Bus Rider Comfort',
       description: 'The stated goal is to foster and facilitate an environment that is friendly for bus riders. To do this, we have decided that adequate seating at bus stops is the best way of improving comfort at a low cost.',
     },
     {
-      icon: 'fas fa-walking icon',
+      icon: faWalking,
       title: 'Reduce Pedestrian Fatalities',
       description: 'The stated goal is to curb Charlotte&apos;s status as being one of the worst pedestrian metros in the United States. In order to tackle this, we aim to pacify the top five pedestrian hotspots where pedestrians have been hit and killed.',
     },
@@ -25,7 +27,7 @@ export default function Projects() {
       <div className="features">
         {projects.map(({ icon, title, description }) => (
           <article>
-            <i className={icon} />
+            <FontAwesomeIcon icon={icon} className="icon" />
             <div className="content">
               <h3>{title}</h3>
               <p>{description}</p>
