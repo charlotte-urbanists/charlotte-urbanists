c277261a4bc84517e8d8b8fc1ab4c380be5536a6
Rahul <rahul@rsun.dev>
2022-02-20 11:58:05 -0500

    minor tag changes

--- src/components/TitleBar.jsx
+++ src/components/TitleBar.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
 import { Link } from 'gatsby';
-
-import '../styles/css/main.css';
-import '../styles/css/fontawesome-all.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faInstagram } from '@fortawesome/fontawesome-free-brands';
 
 export default function TitleBar() {
   return (
-    <nav id="header" className="inner">
+    <header id="header" className="inner">
       {/* TODO: fix navbar CSS */}
       <Link to="/" className="logo">Charlotte Urbanists</Link>
       <ul className="icons">
-        <li><a href="https://twitter.com/CLT_Urbanists" target="_blank" rel="noopener noreferrer" className="icon brands fa-twitter"><span className="label">Twitter</span></a></li>
-        <li><a href="https://www.instagram.com/clt_urbanists/" target="_blank" rel="noopener noreferrer" className="icon brands fa-instagram"><span className="label">Instagram</span></a></li>
+        <li>
+          <a
+            href="https://twitter.com/CLT_Urbanists"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Charlotte Urbanists Twitter"
+            className="icon"
+          >
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.instagram.com/clt_urbanists/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Charlotte Urbanists Instagram"
+            className="icon"
+          >
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+        </li>
+
       </ul>
-    </nav>
+    </header>
   );
 }
