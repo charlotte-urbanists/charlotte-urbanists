a6c016080d13b324e5cfc535192001b967ca2258
Rahul <rahul@rsun.dev>
2022-02-20 11:53:24 -0500

    SASSify the site. goodbye CSS

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
-    plugins: [
-        "gatsby-plugin-react-helmet",
-        "gatsby-plugin-image",
-        "gatsby-plugin-sharp",
-        "gatsby-transformer-sharp",
-    ],
-}
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sass',
+  ],
+};
