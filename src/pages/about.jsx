6eb8a5c3b2706630dea824fcad893cfdd56b59ad
Rahul <rahul@rsun.dev>
2022-02-20 11:57:49 -0500

    added more info in about us page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,18 +2,17 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import { Link } from 'gatsby';
 
-import '../styles/css/main.css';
-import '../styles/css/fontawesome-all.min.css';
+import '../styles/sass/main.scss';
 import Layout from '../components/Layout';
 
 export default function AboutUs() {
   const goals = [
-    {title: "Walkability", description: "Charlotte Urbanists believe that great cities are cities where people can meet their daily needs via walking or cycling in a safe and pleasant manner."},
-    {title: "Transit Wealth", description: "Charlotte Urbanists believe that great cities have transit that is prioritized over private motor vehicles, is pleasant to use, and have reliable, frequent service."},
-    {title: "Public Spaces", description: "Charlotte Urbanists believe that great cities are rich in public spaces and pathways where every citizen is allowed to dwell and inhabit without expectation of payment."},
-    {title: "Housing", description: "Charlotte Urbanists believe that removing barriers for dense, multi-family housing is an essential element in the path to providing Charlotteans with affordable, flexible housing."},
-  ]
-    
+    { title: 'Walkability', description: 'Charlotte Urbanists believe that great cities are cities where people can meet their daily needs via walking or cycling in a safe and pleasant manner.' },
+    { title: 'Transit Wealth', description: 'Charlotte Urbanists believe that great cities have transit that is prioritized over private motor vehicles, is pleasant to use, and have reliable, frequent service.' },
+    { title: 'Public Spaces', description: 'Charlotte Urbanists believe that great cities are rich in public spaces and pathways where every citizen is allowed to dwell and inhabit without expectation of payment.' },
+    { title: 'Housing', description: 'Charlotte Urbanists believe that removing barriers for dense, multi-family housing is an essential element in the path to providing Charlotteans with affordable, flexible housing.' },
+  ];
+
   return (
     <>
       <Helmet>
@@ -22,16 +21,38 @@ export default function AboutUs() {
       <Layout>
         <section>
           <header className="main">
-            <h1>Our Values</h1>
+            <h2>Our Mission</h2>
           </header>
+          <p>
+            The Charlotte Urbanists Mission is to combat destructive suburban norms by advocating
+            for sustainable, sensible, and equitable urban practices and policies.
+          </p>
         </section>
-        <dl>
-          {goals.map(({title, description}) => (
-            <><dt>{title}</dt><dd>{description}</dd></>
-          ))}
-        </dl>
+        <section>
+          <header className="main">
+            <h2>Our Vision</h2>
+          </header>
+          <p>
+            The Charlotte Urbanists Vision is for Charlotte to be a thriving city with
+            functioning transit,
+            zero pedestrian fatalities, and affordable housing for all members of the community.
+          </p>
+        </section>
+        <section>
+          <header className="main">
+            <h2>Our Values</h2>
+          </header>
+          <dl>
+            {goals.map(({ title, description }) => (
+              <>
+                <dt>{title}</dt>
+                <dd>{description}</dd>
+              </>
+            ))}
+          </dl>
+        </section>
         <hr className="major" />
-        <Link to="/"><button type>Go back home</button></Link>
+        <Link to="/" className="button big">Go back home</Link>
       </Layout>
     </>
   );
